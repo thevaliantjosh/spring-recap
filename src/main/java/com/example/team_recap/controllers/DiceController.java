47e4c83dbf60cca46afb72e18bcbb5e715d6f25f
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-11-26 11:29:51 -0600

    Dice Controller Completed

--- src/main/java/com/example/team_recap/controllers/DiceController.java
+++ src/main/java/com/example/team_recap/controllers/DiceController.java
@@ -1,6 +1,7 @@
 package com.example.team_recap.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +14,14 @@ public class DiceController {
     public String dicePage(){
         return "roll-dice";
     }
-//
-//    @GetMapping("/{number}")
-//    public String diceRoll(@PathVariable byte number){
-//
-//    }
+
+    @GetMapping("/{number}")
+    public String diceRoll(@PathVariable byte number, Model model){
+        model.addAttribute("number", number);
+        model.addAttribute("random", (int) Math.ceil(Math.random()*6));
+        return "roll-dice";
+
+    }
 
 
 }
